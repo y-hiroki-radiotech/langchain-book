d1b22da3a58d11d150bc208c7576a89667b73e51
y-hiroki-radiotech <bnndd439@ybb.ne.jp>
2024-02-24 02:52:06 +0000

    chat openai

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
+import os
+
 import streamlit as st
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+load_dotenv()
 
 st.title("langchain-streamlit-app")
 
-prompt = st.chat_input("What is up")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("What is up?")
 
 if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("user"):
         st.markdown(prompt)
     
     with st.chat_message("assistant"):
-        response = "こんにちは"
-        st.markdown(response)
+        chat = ChatOpenAI(
+            model_name=os.environ["OPENAI_API_MODEL"],
+            temperature=os.environ["OPENAI_API_TEMPERATURE"]
+            )
+        messages = [HumanMessage(content=prompt)]
+        response = chat(messages)
+        st.markdown(response.content)
+        
+    st.session_state.messages.append({"role": "assistant", "content": response})
