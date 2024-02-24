ed46ba03a8f37f38ff98c468c6cc6c9a1c4788dd
y-hiroki-radiotech <73370348+y-hiroki-radiotech@users.noreply.github.com>
2024-02-24 14:33:47 +0900

    Add langchain-streamlit-app

--- app.py
+++ app.py
@@ -2,10 +2,40 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
 
-load_dotenv()
+load_dotenv() # .envの環境変数を設定する
+
+
+def create_agent_chain():
+    chat = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        streaming=True,
+    )
+
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    }
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(
+        tools,
+        chat,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
+
+
+if "agent_chain" not in st.session_state:
+    st.session_state.agent_chain = create_agent_chain()
+
 
 st.title("langchain-streamlit-app")
 
@@ -23,14 +53,10 @@ if prompt:
 
     with st.chat_message("user"):
         st.markdown(prompt)
-    
+
     with st.chat_message("assistant"):
-        chat = ChatOpenAI(
-            model_name=os.environ["OPENAI_API_MODEL"],
-            temperature=os.environ["OPENAI_API_TEMPERATURE"]
-            )
-        messages = [HumanMessage(content=prompt)]
-        response = chat(messages)
-        st.markdown(response.content)
-        
+        callback = StreamlitCallbackHandler(st.container())
+        response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
+        st.markdown(response)
+
     st.session_state.messages.append({"role": "assistant", "content": response})
