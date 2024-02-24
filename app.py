5f96e05ac2dc644395fad18495c0fc7238d1ab38
y-hiroki-radiotech <bnndd439@ybb.ne.jp>
2024-02-24 02:08:18 +0000

    chat_message

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("langchain-streamlit-app")
+
+prompt = st.chat_input("What is up")
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        response = "こんにちは"
+        st.markdown(response)
